c0f8bd6d1ad3726874cde26d879d93942c418082
Ossaija ThankGod <ossaija.thankgod@andela.com>
2019-03-14 11:13:39 +0100

    Add algorithm to capture a frame from streaming video

---
+++ src/picture-manager.js
@@ -0,0 +1,35 @@
+/**
+ * Capture a frame from the streaming video
+ * @param {canvas object} canvas canvas on which the captured frame is manipulated
+ * @param {video object} video html element displaying captured media from user device
+ * @param {number} height height of canvas
+ * @param {number} width width of canvas
+ */
+export const takePicture = ({canvas, video, width, height, photo}) => {
+  const context = canvas.getContext("2d");
+  if(width && height) {
+    canvas.height = height;
+    canvas.width = width;
+    context.drawImage(video, 0, 0, width, height);
+
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+  }else {
+    clearPhoto(canvas, photo)
+  }
+}
+
+/**
+ * Move captured frame from canvas to an image element
+ * @param {object} canvas canvas on which the captured frame is manipulated
+ * @param {object} photo html element on to display captured frame turned image
+ */
+export const clearPhoto = (canvas, photo) => {
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const data = canvas.toDataURL("image/png");
+  photo.setAttribute("src", data);
+}
+
--- src/picture.js
+++ src/picture.js
@@ -10,13 +10,19 @@ let   startbutton = null;
 const width       = 320;
 const height      = 0;
 
-
+/**
+ * start video streaming
+ */
 const startUp = () => {
   startBtn = document.getElementById("startbutton");
   canvas   = document.getElementById("canvas");
   video    = document.getElementById("video");
   photo    = document.getElementById("photo");
 
+  /**
+   * settings that media captured from user device
+   * need to abide by
+   */
   const constraint = {
     video: {
       width: { ideal: 600 },
@@ -25,12 +31,19 @@ const startUp = () => {
     },
   }
 
+ /**
+  * get stream data from media devices on a
+  * user system
+  */
   navigator.mediaDevices.getUserMedia(constraint)
     .then(stream => {
       video.srcObject = stream;
       video.play();
     }).catch(err => console.log(`An error occurred ${err}`));
 
+  /**
+   * set up video window
+   */
   video.addEventListener("canplay", ev => {
     if(!streaming) {
       height = video.videoHeight / (videoWidth/width);
@@ -41,6 +54,18 @@ const startUp = () => {
       video.setAttribute("width", width);
       streaming = true;
     }
-  }, false);``
+  }, false);
+
+  /**
+   * response to user click interaction with
+   * a button in viewport
+   */
+  startbutton.addEventListener("click", ev => {
+    takePicture();
+    ev.preventDefault();
+  }, false);
+
+
+  clearphoto();
 }
 
